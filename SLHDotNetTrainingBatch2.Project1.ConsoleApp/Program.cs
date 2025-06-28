be0f8e7fda6e5057bd19bb2752b27162e9990459
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-06-28 22:20:34 +0630

    feat: Sale + Sale Detail

--- SLHDotNetTrainingBatch2.Project1.ConsoleApp/Program.cs
+++ SLHDotNetTrainingBatch2.Project1.ConsoleApp/Program.cs
@@ -1,49 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SLHDotNetTrainingBatch2.Project1.ConsoleApp;
 
-Console.WriteLine("Welcome, Mini POS!");
+SaleFeatureService saleFeatureService = new SaleFeatureService();
+saleFeatureService.Execute();
 
-Result:
-Console.WriteLine("Menus");
-Console.WriteLine("------------------------------");
-Console.WriteLine("1. Product");
-Console.WriteLine("2. Sale");
-Console.WriteLine("3. Sale Detail");
-Console.WriteLine("4. Exit");
-Console.WriteLine("------------------------------");
+//Console.WriteLine("Welcome, Mini POS!");
 
-Console.Write("Choose Menu: ");
-string result = Console.ReadLine()!;
-bool isInt = int.TryParse(result, out int no);
-if (!isInt)
-{
-    Console.WriteLine("Invalid Product Menu. Please choose 1 to 4.");
-    goto Result;
-}
+//Result:
+//Console.WriteLine("Menus");
+//Console.WriteLine("------------------------------");
+//Console.WriteLine("1. Product");
+//Console.WriteLine("2. Sale");
+//Console.WriteLine("3. Sale Detail");
+//Console.WriteLine("4. Exit");
+//Console.WriteLine("------------------------------");
 
-EnumMenu menu = (EnumMenu)no;
-switch (menu)
-{
-    case EnumMenu.Product:
-        ProductService productService = new ProductService();
-        productService.Execute();
-        goto Result;
-    case EnumMenu.Sale:
-        SaleService saleService = new SaleService();
-        saleService.Execute();
-        goto Result;
-    case EnumMenu.SaleDetail:
-        SaleDetailService saleDetailService = new SaleDetailService();
-        saleDetailService.Execute();
-        goto Result;
-    case EnumMenu.Exit:
-        goto End; 
-    case EnumMenu.None:
-    default:
-        Console.WriteLine("Invalid Menu. Please choose 1 to 5.");
-        goto Result;
-}
+//Console.Write("Choose Menu: ");
+//string result = Console.ReadLine()!;
+//bool isInt = int.TryParse(result, out int no);
+//if (!isInt)
+//{
+//    Console.WriteLine("Invalid Product Menu. Please choose 1 to 4.");
+//    goto Result;
+//}
 
-End:
-Console.WriteLine("Exiting Mini POS...");
+//EnumMenu menu = (EnumMenu)no;
+//switch (menu)
+//{
+//    case EnumMenu.Product:
+//        ProductService productService = new ProductService();
+//        productService.Execute();
+//        goto Result;
+//    case EnumMenu.Sale:
+//        SaleService saleService = new SaleService();
+//        saleService.Execute();
+//        goto Result;
+//    case EnumMenu.SaleDetail:
+//        SaleDetailService saleDetailService = new SaleDetailService();
+//        saleDetailService.Execute();
+//        goto Result;
+//    case EnumMenu.Exit:
+//        goto End; 
+//    case EnumMenu.None:
+//    default:
+//        Console.WriteLine("Invalid Menu. Please choose 1 to 5.");
+//        goto Result;
+//}
+
+//End:
+//Console.WriteLine("Exiting Mini POS...");
 //Console.ReadLine();
