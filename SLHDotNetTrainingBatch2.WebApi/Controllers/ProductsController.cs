b12a030ffd5b75568bc6da2214e994c55a4fb58a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-07-13 03:24:07 +0630

    feat: add asp.net core web api

---
+++ SLHDotNetTrainingBatch2.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SLHDotNetTrainingBatch2.WebApi.Controllers
+{
+    // api/product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // process
+            // data
+            return Ok("Get");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            // process
+            // data
+            return Ok("Create");
+        }
+
+        [HttpPut]
+        public IActionResult Upsert()
+        {
+            // process
+            // data
+            return Ok("Upsert");
+        }
+
+        [HttpPatch]
+        public IActionResult Update()
+        {
+            // process
+            // data
+            return Ok("Update");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            // process
+            // data
+            return Ok("Delete");
+        }
+    }
+}
