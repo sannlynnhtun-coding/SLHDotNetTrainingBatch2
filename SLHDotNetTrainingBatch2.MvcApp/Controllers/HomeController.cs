eb6c5f334726e0f6b18e4ccfa41876aff38c26cc
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-09-01 11:15:02 +0630

    feat: add create mvc project

---
+++ SLHDotNetTrainingBatch2.MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SLHDotNetTrainingBatch2.MvcApp.Models;
+using System.Diagnostics;
+
+namespace SLHDotNetTrainingBatch2.MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
